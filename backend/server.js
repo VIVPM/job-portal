c5efcb2dc60c6e43295c1c3541ccb73294acdd90
VIVEK P M <vivpm99@gmail.com>
2024-05-16 11:05:54 +0530

    apis

--- backend/server.js
+++ backend/server.js
@@ -37,12 +37,12 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 // Setting up middlewares
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  credentials: true,            //access-control-allow-credentials:true
-  optionSuccessStatus: 200
-}
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: 'http://localhost:3000',
+//   credentials: true,            //access-control-allow-credentials:true
+//   optionSuccessStatus: 200
+// }
+app.use(cors());
 app.use(express.json());
 app.use(passportConfig.initialize());
 
