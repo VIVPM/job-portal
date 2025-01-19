9cc16007d3721054f4b103467a4d6646e71bf075
VIVPM <vivpm99@gmail.com>
2025-01-19 13:04:54 +0530

    news

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,7 @@ const port = process.env.port || 4444;
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use('/', express.static('/build'))
 
 // Setting up middlewares
 // const corsOptions = {
