5c0275bc0bb55d08ec1777595e5b16b869821a57
VIVPM <vivpm99@gmail.com>
2025-05-21 15:47:24 +0530

    new_python6

--- backend/app.py
+++ backend/app.py
@@ -135,4 +135,6 @@ async def resume_checker(
 # ------------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
